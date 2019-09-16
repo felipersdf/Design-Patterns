f32d2814ae0265d0845990a7e91ac23cf677a1bc
Felipe da Silva Rodrigues <20171370006@ad.ifpb.edu.br>
2019-09-16 17:26:29 -0300

    Dando nomes aos bois

---
+++ InformationExpert/InformationExpert/src/Locadora.java
@@ -0,0 +1,28 @@
+public class Locadora {
+    public static void main(String[] args) {
+
+        Fita fita1 = new Fita("O Rei Leao", Tipo.infantil);
+        Fita fita2 = new Fita("Os Vingadores", Tipo.lancamento);
+        Fita fita3 = new Fita("Titanic", Tipo.normal);
+
+        Cliente cli1 = new Cliente("Felipe");
+        Cliente cli2 = new Cliente("Maria");
+
+        Aluguel aluguel1 = new Aluguel(fita1, 5);
+        Aluguel aluguel2 = new Aluguel(fita2, 3);
+        Aluguel aluguel3 = new Aluguel(fita3, 2);
+        
+        
+        cli1.adicionaAluguel(aluguel1);
+        cli1.adicionaAluguel(aluguel2);
+        cli1.adicionaAluguel(aluguel3);
+
+        cli2.adicionaAluguel(aluguel3);
+        cli2.adicionaAluguel(aluguel2);
+
+        System.out.println(cli1.extrato());
+        System.out.println(cli2.extrato());
+
+
+    }
+}
